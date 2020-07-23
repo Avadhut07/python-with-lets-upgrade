3d54e1367591d17a97c14cb2e7b11710bb52288a
Avadhut07 <68601790+Avadhut07@users.noreply.github.com>
2020-07-23 16:07:55 +0530

    Add files via upload

---
+++ Assignment No. 4.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[3]:
+
+
+#ASSIGNMENT NO. 4
+#QUASTION NO. 1
+#FIND THE INDEX VALUE OF GIVEN STRING
+#"what we think we become ; we are Python programmers "
+
+str1 = ("what we think we become; we are Python programmer")
+
+
+
+print("Index Value is",str1.index("we"))
+
+
+# In[5]:
+
+
+#QUASTION NO.2
+# USE ISLOWER METHOD WITH DIFFERENT KIND OF STRING
+#1)islower
+#1.1)
+lst = ("Hi, How are you, Are you fine.")
+
+print(lst.islower())
+
+
+# In[16]:
+
+
+#1.2)
+
+lst2= ("hi, how are you, are you fine.")
+
+print(lst2.islower())
+
+
+# In[18]:
+
+
+#2)isupper
+
+str1 = ("hi you wan't to paly footboll with me")
+
+print(lst2.isupper())
+
+
+# In[22]:
+
+
+str12 = ("HI YOU WAN'T TO PALY FOOTBOLL WITH ME")
+
+print(str12.isupper())
+
+
+# In[ ]:
+
+
+
+
